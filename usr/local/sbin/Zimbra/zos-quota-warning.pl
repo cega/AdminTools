cadf41a95c87dbb42e56279449dd1682031cb4d8
B-LUC Consulting <consult@btoy1.net>
2014-05-14 14:24:01 -0400

    Corrected/added the license reference

--- usr/local/sbin/Zimbra/zos-quota-warning.pl
+++ usr/local/sbin/Zimbra/zos-quota-warning.pl
@@ -1,9 +1,18 @@
 #!/usr/bin/perl
 ################################################################
+# (c) Copyright 2012 B-LUC Consulting and Thomas Bullinger
 #
-# $Id: xwa-quota-warning.pl,v 1.2 2011/10/24 02:54:54 xdefenders Exp $
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
 #
-# (c) Copyright 2006 xDefenders, inc. and Thomas Bullinger
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
 ################################################################
 # quota_zimbra.pl :     this perl script allows the user to know if his current mail quota is a warning state...
 #                       the state is determinated by the thresholds (in percent) you set
